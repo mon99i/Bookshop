204e00fce327dbfd141e3d04221e147d6c61c6da
Kim Wongi <woongi3012@gmail.com>
2024-06-07 14:57:08 +0900

    feat : selected 여부에 따른 기능 수정

--- controller/cart-controller.js
+++ controller/cart-controller.js
@@ -2,8 +2,6 @@ const ensureAuthorization = require('../auth'); // 인증 모듈
 const conn = require('../mariadb');
 const {StatusCodes} = require('http-status-codes');
 const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-dotenv.config();
 
 // 장바구니 담기
 const addToCart = (req, res) => {
@@ -49,10 +47,16 @@ const getCartItems = (req, res) => {
         });
     } else {
         let sql = `SELECT cartItems.id, book_id, title, summary, quantity, price
-                    FROM cartItems LEFT JOIN books
-                    ON cartItems.book_id = books.id
-                    WHERE user_id=? AND cartItems.id IN (?)`;
-        let values = [authorization.id, selected];
+                FROM cartItems LEFT JOIN books
+                ON cartItems.book_id = books.id
+                WHERE user_id=?`;
+        let values = [authorization.id];
+
+        if(selected) { // 주문서 작성 시 '선택한 장바구니 목록 조회'
+            sql += ` AND cartItems.id IN (?)`;
+            values.push(selected);
+        }
+        
         conn.query(sql, values,
             (err, results) => {
                 if(err) {
