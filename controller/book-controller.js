8b5cb86db539328bdc61e54cb041218166a7dc81
Kim Wongi <woongi3012@gmail.com>
2024-06-07 17:03:41 +0900

    feat #1 : pagination 추가 feat #2 : authorization 추가

--- controller/book-controller.js
+++ controller/book-controller.js
@@ -1,8 +1,11 @@
+const ensureAuthorization = require('../auth'); // 인증 모듈
+const jwt = require('jsonwebtoken');
 const {StatusCodes} = require('http-status-codes');
 const conn = require('../mariadb');
 
 // (카테고리 별, 신간 여부) 전체 도서 목록 조회
 const allBooks = (req, res) => {
+    let allBooksRes = {};
     let { category_id, news, limit, currentPage } = req.query;
 
     // limit : page 당 도서 수 
@@ -10,7 +13,7 @@ const allBooks = (req, res) => {
     // offset : limit * (currentPage-1) 
     let offset = limit * (currentPage-1);
 
-    let sql = "SELECT *, (SELECT count(*) FROM likes WHERE liked_book_id = books.id) AS likes FROM books";
+    let sql = "SELECT SQL_CALC_FOUND_ROWS *, (SELECT count(*) FROM likes WHERE liked_book_id = books.id) AS likes FROM books";
     let values = [];
     if(category_id && news) {
         sql += " WHERE category_id=? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()";
@@ -24,46 +27,99 @@ const allBooks = (req, res) => {
 
     sql += " LIMIT ? OFFSET ?";
     values.push(parseInt(limit), offset);
-
     conn.query(sql, values,
         (err, results) => {
-        if(err) {
-            console.log(err);
-            return res.status(StatusCodes.BAD_REQUEST).end();
-        }
-
-        if(results.length)
-            return res.status(StatusCodes.OK).json(results);
-        else 
-            return res.status(StatusCodes.NOT_FOUND).end();
-    })
-};
-
-// 개별 도서 조회
-const bookDetail = (req, res) => {
-    let {user_id} = req.body;
-    let book_id = req.params.id;
-
-    let sql = `SELECT *,
-                    (SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes,
-                    (SELECT EXISTS (SELECT * FROM likes WHERE user_id=? AND liked_book_id=?)) AS liked
-                FROM books
-                LEFT JOIN category
-                ON books.category_id = category.category_id
-                WHERE books.id=?;`;
-    let values = [user_id, , book_id, book_id];
-    conn.query(sql, values,
+            if(err) {
+                console.log(err);
+                // return res.status(StatusCodes.BAD_REQUEST).end();
+                }
+                console.log(results);
+                if(results.length)
+                    allBooksRes.books = results;
+                else 
+                    return res.status(StatusCodes.NOT_FOUND).end();
+        })
+        
+    sql = "SELECT found_rows()";
+    conn.query(sql,
         (err, results) => {
             if(err) {
                 console.log(err);
                 return res.status(StatusCodes.BAD_REQUEST).end();
             }
 
-            if(results[0])
-                return res.status(StatusCodes.OK).json(results[0]);
-            else 
-                return res.status(StatusCodes.NOT_FOUND).end();
-        })
+            let pagination = {};
+            pagination.currentPage = parseInt(currentPage);
+            pagination.totalCount = results[0]["found_rows()"];
+
+            allBooksRes.pagination = pagination;
+
+            return res.status(StatusCodes.OK).json(allBooksRes);
+    })
+};
+
+// 개별 도서 조회
+const bookDetail = (req, res) => {
+
+    // 로그인 상태 X => liked 제외
+    // 로그인 상태 O => liked 추가
+
+    let authorization = ensureAuthorization(req, res);
+
+    if(authorization instanceof jwt.TokenExpiredError) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            "message" : "로그인 세션이 만료되었습니다. 다시 로그인하세요"
+        });
+    } else if (authorization instanceof jwt.JsonWebTokenError) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            "message" : "잘못된 토큰입니다."
+        });
+    } else if (authorization instanceof ReferenceError) {
+        let book_id = req.params.id;
+
+        let sql = `SELECT *,
+                        (SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes
+                    FROM books
+                    LEFT JOIN category
+                    ON books.category_id = category.category_id
+                    WHERE books.id=?;`;
+        let values = [book_id];
+        conn.query(sql, values,
+            (err, results) => {
+                if(err) {
+                    console.log(err);
+                    return res.status(StatusCodes.BAD_REQUEST).end();
+                }
+
+                if(results[0])
+                    return res.status(StatusCodes.OK).json(results[0]);
+                else 
+                    return res.status(StatusCodes.NOT_FOUND).end();
+            })
+    } else {
+        let book_id = req.params.id;
+
+        let sql = `SELECT *,
+                        (SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes,
+                        (SELECT EXISTS (SELECT * FROM likes WHERE user_id=? AND liked_book_id=?)) AS liked
+                    FROM books
+                    LEFT JOIN category
+                    ON books.category_id = category.category_id
+                    WHERE books.id=?;`;
+        let values = [authorization.id, book_id, book_id];
+        conn.query(sql, values,
+            (err, results) => {
+                if(err) {
+                    console.log(err);
+                    return res.status(StatusCodes.BAD_REQUEST).end();
+                }
+
+                if(results[0])
+                    return res.status(StatusCodes.OK).json(results[0]);
+                else 
+                    return res.status(StatusCodes.NOT_FOUND).end();
+            })
+        }
 };
 
 module.exports = {
