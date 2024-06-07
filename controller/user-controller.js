a9f7b8c085662c97110c6242c32217619f2c3db8
Kim Wongi <woongi3012@gmail.com>
2024-06-07 10:58:37 +0900

    CHORE - user-controller / jwt-id 구문 수정

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -45,6 +45,7 @@ const login = (req, res) => {
 
             if(loginUser && loginUser.password == hashPassword) {
                 const token = jwt.sign({
+                    id : loginUser.id,
                     email : loginUser.email
                 }, process.env.PRIVATE_KEY, {
                     expiresIn : '5m',
